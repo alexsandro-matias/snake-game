e6a58104153a1ad00bdb7b96749be7111e2c6431
alexsandro-matias <matiasalexsandro@gmail.com>
2020-10-14 09:36:18 -0300

    Criadas mais classes.

--- src/snake/Application.java
+++ src/snake/Application.java
@@ -1,6 +1,6 @@
 package snake;
 
-public class App {
+public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("teste");
--- src/snake/GameWindon.java
+++ src/snake/GameWindon.java
@@ -3,5 +3,11 @@ package snake;
 import javax.swing.JFrame;
 
 public class GameWindon extends JFrame {
+	
+	
+
+	public GameWindon() {
+
+	}
 
 }
